e5a1154cf7c13952d9b2e73ea0121489b39585f2
Asutosh2072 <asutoshsahoo072@gmail.com>
2025-01-18 17:19:38 +0530

    change in script file

--- script.js
+++ script.js
@@ -22,78 +22,94 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
-        // Handle popup close and restart the carousel
+    // Handle popup close and restart the carousel
     if (closeButton && popup) {
         closeButton.addEventListener('click', function () {
-            popup.style.display = 'none'; // Close the popup
+           if(popup) {
+             popup.style.display = 'none'; // Close the popup
             if (carouselElement) {
-             new bootstrap.Carousel(carouselElement).cycle() // Restart the carousel cycle
+               new bootstrap.Carousel(carouselElement).cycle() // Restart the carousel cycle
             }
             if(carouselElement1){
-              new bootstrap.Carousel(carouselElement1).cycle()
+               new bootstrap.Carousel(carouselElement1).cycle()
             }
-
+           }
         });
     }
 
+     // Form submission handling
+     if (form) {
+        //Initially disable the button
+         if(submitBtn){
+             submitBtn.setAttribute('disabled', true);
+         }
 
-    // Form submission handling
-    if (form) {
-         //Initially disable the button
-         submitBtn.setAttribute('disabled', true);
 
-        recaptchaCheckbox.addEventListener('change',function(){
-            console.log("checkbox changed")
-            if (this.checked){
-                submitBtn.removeAttribute('disabled')
-                console.log("button enabled")
-            }else{
-                submitBtn.setAttribute('disabled', true)
-                console.log("button disabled")
-            }
-        })
+         if(recaptchaCheckbox){
+            recaptchaCheckbox.addEventListener('change',function(){
+                console.log("checkbox changed")
+                if (this.checked){
+                    if(submitBtn){
+                      submitBtn.removeAttribute('disabled')
+                     console.log("button enabled")
+                    }
+
+                }else{
+                    if(submitBtn){
+                      submitBtn.setAttribute('disabled', true)
+                       console.log("button disabled")
+                    }
+                }
+            })
+         }
 
         form.addEventListener('submit', function(event) {
-            event.preventDefault();  // Prevent the default form submission
-            console.log("form submission started")
-             if (!recaptchaCheckbox.checked){
-                alert("Please confirm you are not a bot")
-                return;
-            }
+          event.preventDefault();  // Prevent the default form submission
+          console.log("form submission started")
+           if (recaptchaCheckbox && !recaptchaCheckbox.checked){
+              alert("Please confirm you are not a bot")
+               return;
+          }
 
             fetch(form.action, {
               method: 'POST',
               headers: {
-                'Content-Type': 'application/x-www-form-urlencoded', // or 'application/json'
+                 'Content-Type': 'application/x-www-form-urlencoded', // or 'application/json'
               },
               body: new URLSearchParams(new FormData(form)).toString(), // or JSON.stringify(formData)
-            })
-            .then(response => {
-              if (response.ok) {
+             })
+             .then(response => {
+               if (response.ok) {
                   return response.text();
-              } else {
-                  throw new Error('Network response was not ok.');
-              }
+               } else {
+                   throw new Error('Network response was not ok.');
+                }
+             })
+             .then(data => {
+                 alert('Form submission successful: ' + data);
+                 console.log(data)
+                 form.reset();
+                 if(submitBtn){
+                   submitBtn.setAttribute('disabled', true);
+                 }
+                 if(recaptchaCheckbox){
+                  recaptchaCheckbox.checked = false;
+                 }
+
             })
-            .then(data => {
-                alert('Form submission successful: ' + data);
-                console.log(data)
-                form.reset();
-                submitBtn.setAttribute('disabled', true);
-                recaptchaCheckbox.checked = false;
-            })
-              .catch(error => {
-                  console.error('There was a problem with the fetch operation:', error);
+               .catch(error => {
+                   console.error('There was a problem with the fetch operation:', error);
                   alert('Form submission failed');
-              });
-        });
-    }
-  document.querySelectorAll(".btn-dark").forEach(button => {
-      button.addEventListener("click", function () {
+             });
+         });
+      }
+
+    document.querySelectorAll(".btn-dark").forEach(button => {
+       button.addEventListener("click", function () {
         const targetId = this.getAttribute("data-target");
         const targetElement = document.getElementById(targetId);
         if (targetElement) {
-          targetElement.scrollIntoView({ behavior: "smooth" });
+             targetElement.scrollIntoView({ behavior: "smooth" });
         }
       });
     });
@@ -116,18 +132,19 @@ function redirectToLinkedIn() {
 // Function to close the popup
 function closePopup() {
     const popup = document.getElementById('popup');
-    if (popup) {
-        popup.style.display = 'none'; // Hide the popup
+   if (popup) {
+      popup.style.display = 'none'; // Hide the popup
     }
 }
 
 document.addEventListener('DOMContentLoaded', function () {
     const heroImage = document.querySelector('.hero-image img');
-
-    window.addEventListener('scroll', function () {
-        const scrollPos = window.scrollY;
-        heroImage.style.transform = `translateY(${-scrollPos / 6}px)`;
-    });
+      if(heroImage){
+         window.addEventListener('scroll', function () {
+             const scrollPos = window.scrollY;
+             heroImage.style.transform = `translateY(${-scrollPos / 6}px)`;
+         });
+     }
 });
 
 
@@ -136,39 +153,39 @@ document.addEventListener('DOMContentLoaded', function () {
 
 document.addEventListener("DOMContentLoaded", () => {
     // Function to animate the count for each element
-    function animateCount(element, targetValue, duration) {
-      let startValue = 0;
-      let startTime = null;
-  
+   function animateCount(element, targetValue, duration) {
+       let startValue = 0;
+       let startTime = null;
+
       function step(timestamp) {
-        if (!startTime) startTime = timestamp;
-        const elapsed = timestamp - startTime;
-  
-        // Calculate progress (0 to 1)
-        const progress = Math.min(elapsed / duration, 1);
-  
-        // Calculate the current value
-        const currentValue = Math.floor(startValue + (targetValue - startValue) * progress);
-  
-        // Update the element's text with the current value
-        element.textContent = currentValue + "+";
-  
-        // Continue animation or finish
-        if (progress < 1) {
-          requestAnimationFrame(step);
-        }
+           if (!startTime) startTime = timestamp;
+           const elapsed = timestamp - startTime;
+
+            // Calculate progress (0 to 1)
+           const progress = Math.min(elapsed / duration, 1);
+
+            // Calculate the current value
+           const currentValue = Math.floor(startValue + (targetValue - startValue) * progress);
+
+           // Update the element's text with the current value
+          element.textContent = currentValue + "+";
+
+           // Continue animation or finish
+           if (progress < 1) {
+              requestAnimationFrame(step);
+            }
       }
-  
+
       requestAnimationFrame(step);
     }
-  
+
     // Select all elements with the 'data-target' attribute
     const counters = document.querySelectorAll(".count h3");
-  
-    counters.forEach((counter) => {
+
+   counters.forEach((counter) => {
       const targetValue = parseInt(counter.getAttribute("data-target"), 10);
       const duration = 2000; // Duration of animation in milliseconds
-  
+
       animateCount(counter, targetValue, duration);
-    });
+   });
   });
